a14d871366a348f452fb0307f05f9d6689f02e80
vablasSkillChallenge <163834622+vablasSkillChallenge@users.noreply.github.com>
2024-03-18 10:02:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        skills: ['JavaScript', 'React', 'Node'],
+        addSkill: function(skill) {
+        this.skills.push(skill);
+        }
+    };
+    }
